1ebb1004b1926716b0d03201139c3e7b647d00a2
asaarushi2911 <asaarushi2911@gmail.com>
2024-04-04 20:54:05 +0530

    Initial commit

---
+++ ChatBot-Udemy.py
@@ -0,0 +1,10 @@
+pip install streamlit pypdf2 langchain faiss-cpu
+
+import streamlit as st
+
+#Upload PDF files
+st.header("My first Chatbot")
+
+with  st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader(" Upload a PDf file and start asking questions", type="pdf")
